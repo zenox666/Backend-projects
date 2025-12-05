c164e2e8515e5b9e8d8e684ad87722e25674cb3b
Allu Ketan <allusai2007@gmail.com>
2025-12-05 06:22:20 +0530

    Added Dice challenge project

---
+++ 01-Dice-Challenge/index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random()*6+1);
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src","./images/dice"+randomNumber1+".png");
+
+var randomNumber2 = Math.floor(Math.random()*6+1);
+var image2 =  document.querySelectorAll("img")[1];
+image2.setAttribute("src","./images/dice"+randomNumber2+".png");
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="🚩Player 1 wins";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 wins🚩";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!!";
+}
